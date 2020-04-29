b2263f81d0653ab356fe737618dfd2785c8f5080
Myo Thant Kyaw <myothantkyaw21@gmail.com>
2020-04-29 07:49:25 +0630

    Initial Commit

---
+++ scripts/ui.js
@@ -0,0 +1,50 @@
+
+function setupActionListeners() {
+    // // semantic ui
+    // $('.ui.dropdown').dropdown(
+
+    // );
+
+    // $('.ui.tiny.modal').modal('show');
+    // $('.dimmer').dimmer({closable: false}).dimmer('show');
+
+
+
+    var btnInfo = document.getElementById("btnInfo");
+    btnInfo.addEventListener("click", function (evt) {
+        
+        $('.ui.modal').modal('show');
+    });
+
+    // $('.ui.modal').modal({
+    //     onHide: function () {
+            
+    //         threeD.resumeAnimation();
+    //     },
+    //     // onShow: function () {
+    //     //     console.log('shown');
+    //     // }
+    // })
+
+   
+    
+    // $('.dimmer').dimmer({
+    //     onApprove : function() {
+    //       console.log("return falsel.")
+    //       return false; //Return false as to not close modal dialog
+    //     }
+    //   }).dimmer('show');
+      
+
+
+    // When the user clicks anywhere outside of the modal, close it
+    // window.onclick = function (event) {
+    //     if (event.target == modal) {
+    //         hideDialog()
+    //     }
+    // }
+
+    // showDialog();
+}
+
+setupActionListeners() 
